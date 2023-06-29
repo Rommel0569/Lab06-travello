9101777ed4cbca9f4cdd70ca56680d240811bf4e
Rommel Abraham Chambi Velasquez <rchambive@unsa.edu.pe>
2023-06-28 19:25:12 -0500

    Modificando Installed_Apps en settings.py

--- lugares_turisticos/lugares_turisticos/settings.py
+++ lugares_turisticos/lugares_turisticos/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ["127.0.0.1", ".pythonanywhere.com"]
 # Application definition
 
 INSTALLED_APPS = [
+        "destinos.apps.DestinosConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
