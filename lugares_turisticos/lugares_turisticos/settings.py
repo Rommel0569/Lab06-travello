f37c98507a7f9ad8d6b82369d0410c07b8b43694
Rommel Abraham Chambi Velasquez <rchambive@unsa.edu.pe>
2023-06-28 19:22:47 -0500

    Configurando settings.py, sobre todo la ruta y especificaciones para el directorio padre

--- lugares_turisticos/lugares_turisticos/settings.py
+++ lugares_turisticos/lugares_turisticos/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-shs2%9%9g&p7)xv67c8w-(vpi7h$sy^n0+lpd=6#y$isu!@v87
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", ".pythonanywhere.com"]
 
 
 # Application definition
